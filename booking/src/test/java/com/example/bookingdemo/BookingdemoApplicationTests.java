22da5245191002246edef7ece6a2bcd5fe9a2bc7
stodorichkov <steliantodorichkov03@gmail.com>
2026-06-10 22:00:54 +0300

    (booking) setup project

---
+++ booking/src/test/java/com/example/bookingdemo/BookingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
