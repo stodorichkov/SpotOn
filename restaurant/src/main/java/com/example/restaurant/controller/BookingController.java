38cc28dc113f72e08d232cdeaa3967d0da477766
stodorichkov <steliantodorichkov03@gmail.com>
2026-08-08 15:33:35 +0300

    (restaurant) get client bookings

--- restaurant/src/main/java/com/example/restaurant/controller/BookingController.java
+++ restaurant/src/main/java/com/example/restaurant/controller/BookingController.java
@@ -2,6 +2,7 @@ package com.example.restaurant.controller;
 
 import com.example.restaurant.constants.HeaderConstants;
 import com.example.restaurant.model.enums.RoleEnum;
+import com.example.restaurant.model.enums.ServiceEnum;
 import com.example.restaurant.model.payload.response.BookingRestaurantResponse;
 import com.example.restaurant.service.AuthorizationService;
 import com.example.restaurant.service.RestaurantService;
@@ -22,9 +23,12 @@ public class BookingController {
     @ResponseStatus(HttpStatus.OK)
     List<BookingRestaurantResponse> getRestaurants(
             @RequestHeader(HeaderConstants.USER_ROLE) RoleEnum userRoleHeader,
+            @RequestHeader(HeaderConstants.ITERNAL_SERVICE) ServiceEnum service,
+            @RequestHeader(HeaderConstants.ITERNAL_SECRET) String secret,
             @RequestBody List<Long> restaurantIds
     ) {
         this.authorizationService.hasRole(userRoleHeader, RoleEnum.CLIENT);
+        this.authorizationService.hasInternalAccess(secret, service, ServiceEnum.BOOKING);
 
         return this.restaurantService.getBookingRestaurants(restaurantIds);
     }
