d0a9b1ed1cdba4532fadf15da38421b91e2326f4
stodorichkov <steliantodorichkov03@gmail.com>
2026-08-16 19:09:15 +0300

    (api-gateway) cors policy

---
+++ api-gateway/src/main/java/com/example/apigateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+@Configuration
+public class CorsConfig extends CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Collections.singletonList("*"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
