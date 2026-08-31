5b6a02101d8322cc73a30af1eaaa8771a77e0f3d
stodorichkov <steliantodorichkov03@gmail.com>
2026-08-31 21:03:56 +0300

    (booking) remove comments

--- booking/src/main/java/com/example/booking/constants/MessageConstants.java
+++ booking/src/main/java/com/example/booking/constants/MessageConstants.java
@@ -1,23 +1,18 @@
 package com.example.booking.constants;
 
 public class MessageConstants {
-    // Common
     public static final String INTERNAL_SERVER_ERROR = "Internal server error!";
 
-    // Validation
     public static final String BLANK_FIELD = "Field can not be blank.";
     public static final String MIN_GUEST_COUNT = "The minimum guest count is 1.";
     public static final String FUTURE_DATE_TIME = "The booking must be for a future time";
     public static final String SELECT_TABLE = "Please select a table";
 
-    // User
     public static final String ACCESS_DENIED = "Access denied for this account!";
 
-    // Status
     public static final String ADD_STATUS = "Add status: ";
     public static final String STATUS_NOT_FOUND = "Status not found";
 
-    // Booking
     public static final String BOOKING_NOT_FOUND = "Booking not found";
     public static final String TABLE_ALREADY_BOOKED = "Table already booked for this time";
 }
